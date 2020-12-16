199427f9f6c47124c407be98d63284ca4f7c1795
andrius <andrius.kirilovas@cern.ch>
2020-12-16 21:30:00 +0100

    Added libjpeg path path standalone builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ pushd $(scram -a $SCRAM_ARCH list -c $CMSSW_VERSION | tail -1 | sed 's|.* ||') >
   export ROOT_ROOT=$(scram tool tag root_interface ROOT_INTERFACE_BASE)
   export BOOST_ROOT=$(scram tool tag boost BOOST_BASE)
   export PNG_ROOT=$(scram tool tag libpng LIBPNG_BASE)
+  export LIBJPEG_TURBO_ROOT=$(scram tool tag libjpeg-turbo LIBJPEG_TURBO_BASE)
   eval `scram run -sh`
 popd > /dev/null
 

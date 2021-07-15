1b508d4e8cf8fdf69845f0bd63844ffb5f80ff31
Ernesta <ernesta.petraityte@yahoo.com>
2021-07-15 15:28:50 +0200

    Update dqmgui.sh
    Adapted to rpm spec file changed

--- scripts/dqmgui.sh
+++ scripts/dqmgui.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
-BASE="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-CMSSW_VERSION=$(cat $BASE/cmssw_info | sed -n '2 p')
-SCRAM_ARCH=$(cat $BASE/cmssw_info | sed -n '3 p')
+CMSSW_VERSION=$(cat /dqmgui/scripts/cmssw_info | sed -n '2 p')
+SCRAM_ARCH=$(cat /dqmgui/scripts/cmssw_info | sed -n '3 p')
 
 pushd $(scram -a $SCRAM_ARCH list -c $CMSSW_VERSION | tail -1 | sed 's|.* ||') > /dev/null
   eval `scram run -sh`
 popd > /dev/null
 
-python3 $BASE/../python/app.py "$@"
+python3 /dqmgui/python/app.py "$@"

44f74f79ff18828b3558325b9f7a42b7b97a3ec6
Ernesta <ernesta.petraityte@yahoo.com>
2021-07-20 15:47:44 +0200

    Update dqmgui.sh

--- scripts/dqmgui.sh
+++ scripts/dqmgui.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
-BASE=$(pwd)
-CMSSW_VERSION=$(cat $BASE/scripts/cmssw_info | sed -n '2 p')
-SCRAM_ARCH=$(cat $BASE/scripts/cmssw_info | sed -n '3 p')
+CMSSW_VERSION=$(cat /dqmgui/scripts/cmssw_info | sed -n '2 p')
+SCRAM_ARCH=$(cat /dqmgui/scripts/cmssw_info | sed -n '3 p')
 
 pushd $(scram -a $SCRAM_ARCH list -c $CMSSW_VERSION | tail -1 | sed 's|.* ||') > /dev/null
   eval `scram run -sh`
 popd > /dev/null
 
-python3 $BASE/../python/app.py "$@"
+python3 /dqmgui/python/app.py "$@"

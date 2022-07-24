da51a799345890cf2996bd7e20727267f7c91888
chanchana <north1602@gmail.com>
2022-07-24 14:52:12 +0700

    Read config from env vars

--- scripts/alarm-system/config.py
+++ scripts/alarm-system/config.py
@@ -1,12 +1,12 @@
+import os
+
 class Config:
     # Connection config
-    GUI_API_URL = 'http://localhost'
-    GUI_API_PORT = 8889
-    # GUI_API_URL = 'http://127.0.0.1'
-    # GUI_API_PORT = 5000
+    GUI_API_URL = os.environ.get('GUI_API_URL')
+    GUI_API_PORT = os.environ.get('GUI_API_PORT')
     GUI_API_ERROR_DATA_PATH = '/api/v1/archive/0/Global/Online/ALL/00%20Shift/Errors'
-    SOUND_SERVER_URL = None
-    SOUND_SERVER_PORT = None
+    SOUND_SERVER_URL = os.environ.get('SOUND_SERVER_URL')
+    SOUND_SERVER_PORT = os.environ.get('SOUND_SERVER_PORT')
     ALARM_MANAGER_PORT = 8890
 
     # Alarm system parameters
@@ -15,7 +15,7 @@ class Config:
     REMINDER_REBROADCAST_COUNT = 3
     EXECUTION_INTERVAL = 60 * 60 # seconds
     INITIAL_WAIT = 2 # seconds
-    EMAIL_ADDRESSES = ['north1602@gmail.com']
+    EMAIL_ADDRESSES = os.environ.get('EMAIL_ADDRESSES', '').split(',')
     ERROR_QT_STATUS = [300]
 
     # Message content

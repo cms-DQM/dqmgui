aedd3bceaf0d46acd478115e2735bb2f17a335dd
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-03-28 12:09:42 +0100

    Cleanup service and startup scripts

--- scripts/service/start-cleanup.sh
+++ scripts/service/start-cleanup.sh
@@ -5,15 +5,21 @@
 # CMSSW release. This script is invoked by systemd
 # but can also be executed manually
 
-if [ "$USER" != "dqmpro" ]; then
-    echo "This script has to be executed as dqmpro user"
+# Vars replaced during installation
+ALLOWED_USER=dqmpro
+INSTALLATION_DIR=/dqmgui
+CMSSW_BASE_DIR=/dqmdata/dqm_cmssw
+DQMGUI_DATA_DIR=/data/dqmgui
+
+if [ "$USER" != "$ALLOWED_USER" ]; then
+    echo "This script has to be executed as $ALLOWED_USER user"
     exit 1
 fi
 
-CMSSW_SYMLINK=/dqmdata/dqm_cmssw/current_production/src/
-DQMGUI_CLEANUP=/dqmgui/scripts/dqmgui-cleanup.py
+CMSSW_SYMLINK=$CMSSW_BASE_DIR/current_production/src/
+DQMGUI_CLEANUP=$INSTALLATION_DIR/scripts/dqmgui-cleanup.py
 
 cd $CMSSW_SYMLINK
 source /opt/offline/cmsset_default.sh
-eval `scramv1 runtime -sh`
-exec $DQMGUI_CLEANUP -d /data/dqmgui/files/pb/ -c $CMSSW_SYMLINK "$@"
+eval $(scramv1 runtime -sh)
+exec $DQMGUI_CLEANUP -d $DQMGUI_DATA_DIR/files/pb/ -c $CMSSW_SYMLINK "$@"
--- scripts/service/start-gui.sh
+++ scripts/service/start-gui.sh
@@ -5,15 +5,20 @@
 # This script is invoked by systemd but can also
 # be executed manually
 
-if [ "$USER" != "dqmpro" ]; then
-    echo "This script has to be executed as dqmpro user"
+# Vars replaced during installation
+ALLOWED_USER=dqmpro
+INSTALLATION_DIR=/dqmgui
+CMSSW_BASE_DIR=/dqmdata/dqm_cmssw
+DQMGUI_DATA_DIR=/data/dqmgui
+
+if [ "$USER" != "$ALLOWED_USER" ]; then
+    echo "This script has to be executed as $ALLOWED_USER user"
     exit 1
 fi
 
-DQMGUI_PATH=/dqmgui/scripts
+DQMGUI_PATH=$INSTALLATION_DIR/scripts
 cd $DQMGUI_PATH
 source /opt/offline/cmsset_default.sh
-DQMGUI_SCRIPT="dqmgui.sh"
-FULL_PATH="$DQMGUI_PATH/$DQMGUI_SCRIPT"
+DQMGUI_SCRIPT_FILEPATH="$DQMGUI_PATH/dqmgui.sh"
 HOST=$(hostname)
-source $FULL_PATH -p 8889 -r 8 -f /data/dqmgui/files/root/ -d /data/dqmgui/state/ -host $HOST "$@"
+source $DQMGUI_SCRIPT_FILEPATH -p 8889 -r 8 -f $DQMGUI_DATA_DIR/files/root/ -d $DQMGUI_DATA_DIR/state/ -host $HOST "$@"

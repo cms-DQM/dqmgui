1a6dd19f2495716de0fc1a7b80288b6bc437aa12
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-03-28 12:08:03 +0100

    Cleanup

--- scripts/Deployments/monitor.sh
+++ scripts/Deployments/monitor.sh
@@ -3,8 +3,8 @@ echo "SETTING KERBEROS CONFIGURATION"
 if [ -f /etc/secrets/msuccarm.keytab ]; then
     echo "FOUND KEYTABFILE"
     export keytab=/etc/secrets/msuccarm.keytab
-    principal=`klist -k "$keytab" | tail -1 | awk '{print $2}'`
-    kinit $principal -k -t "$keytab" 2>&1 1>& /dev/null
+    principal=$(klist -k "$keytab" | tail -1 | awk '{print $2}')
+    kinit $principal -k -t "$keytab" 2>&1 1>&/dev/null
     if [ $? == 1 ]; then
         echo "Unable to perform kinit."
         echo "If you are installing a DQM GUI on lxplus or any other private machine, please comment lines from this block and proceed as usual"
